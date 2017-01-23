bb35e4d64f216f9132b9173d8e54cd3400b0b52c
polawat phetra <pphetra@gmail.com>
2017-01-23 12:59:13 +0700

    Implement SearchService

---
+++ Investigate/Services/PaginateRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Investigate
+{
+
+	public class PaginateRequest
+	{
+		public int StartPage { set; get;}
+		public int ItemPerPage { set; get;}
+
+		public PaginateRequest() : this(1, 20)
+		{
+		}
+
+		public PaginateRequest(int startPage, int itemPerPage)
+		{
+			this.StartPage = startPage;
+			this.ItemPerPage = itemPerPage;
+		}
+	}
+}
---
+++ Investigate/Services/Report/SearchResult.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Investigate
+{
+
+
+	/**
+	 * 
+	 * {
+     *	"count": 2149,
+     *	"next": "?page=2",
+     *	"previous": null,
+     *	"results": [
+     *	    {
+     *	        "id": 174282,
+     *	        "reportId": 267,
+     *	        "guid": "a02e7e09-d960-4c59-be3c-e6f2a08e9d8d",
+     *	        "reportTypeId": 2,
+     *	        "reportTypeName": "สัตว์ป่วย/ตาย",
+     *	        "date": "2017-01-21T11:02:48Z",
+     *	        "administrationAreaId": 31,
+     *	        "negative": true,
+     *	        "incidentDate": "2017-01-21",
+     *	        "createdBy": "760",
+     *	        "createdByName": "ทนงศักดิ์ ปวงสายใจ",
+     *	        "flag": "",
+     *	        "testFlag": false,
+     *	        "formDataExplanation": "<p>พบ ไก่พื้นเมือง[สัตว์ปีก] ป่วยจำนวน 0 ตัว, ตายจำนวน 0 ตัว จากทั้งหมด 150 ตัว จำนวนเล้า/คอกข้างเคียงที่มีอาการ 0\r\nโดยคาดว่าจะเป็นโรค ห่าไก่ มีอาการดังนี้ แพร่ระบาดรวดเร็ว\r\n</p>\r\n",
+     *	        "renderedOriginalFormData": "<p>พบ ไก่พื้นเมือง[สัตว์ปีก] ป่วยจำนวน 0 ตัว, ตายจำนวน 0 ตัว จากทั้งหมด 150 ตัว จำนวนเล้า/คอกข้างเคียงที่มีอาการ 0\r\nโดยคาดว่าจะเป็นโรค ห่าไก่ มีอาการดังนี้ แพร่ระบาดรวดเร็ว\r\n</p>\r\n",
+     *	        "administrationAreaAddress": "องค์การบริหารส่วนตำบลแม่ทา อำเภอแม่ออน",
+     *	        "firstImageThumbnail": "",
+     *	        "state": 24,
+     *	        "stateCode": "report",
+     *	        "stateName": "Report",
+     *	        "parent": 173801,
+     *	        "tags": null,
+     *	        "reportLocation": [
+     *	            99.2766201,
+     *	            18.5997215
+     *	        ]
+     *	    },
+     *	    
+	 */
+	[JsonObject(MemberSerialization.OptIn)]
+	public class SearchResult
+	{
+
+		public bool Success;
+		public string ErrorMessage;
+
+		[JsonProperty("count")]
+		public int Count;
+
+		[JsonProperty("next")]
+		public string NextUri;
+
+		[JsonProperty("results")]
+		public SearchItem[] results;
+
+	}
+
+	[JsonObject(MemberSerialization.OptIn)]
+	public class SearchItem
+	{
+		[JsonProperty("id")]
+		public int Id;
+
+		[JsonProperty("date")]
+		public DateTime Date;
+
+		[JsonProperty("administrationAreaAddress")]
+		public String AdministrationAreaName;
+
+		[JsonProperty("createByName")]
+		public String CreateByName;
+
+		[JsonProperty("renderedOriginalFormData")]
+		public String RendererFormData;
+	}
+}
