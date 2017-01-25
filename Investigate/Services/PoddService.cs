880802cbef9ef55a1a4f52990570c3b14e888a1f
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2017-01-25 20:47:51 +0700

    Add incidents list header

--- Investigate/Services/PoddService.cs
+++ Investigate/Services/PoddService.cs
@@ -18,7 +18,7 @@ namespace Investigate
 		public PoddService()
 		{
 			ServerUrl = "https://api.cmonehealth.org";
-			ServerUrl = "http://10.0.3.37:32770";
+			//ServerUrl = "http://10.0.3.37:32770";
 		}
 			
 
