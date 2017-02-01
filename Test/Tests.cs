d5addfac2d813285a15fa96b46acf04dcc94159e
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2017-02-01 13:51:36 +0700

    Add test files

---
+++ Test/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            var viewModel = new
+            Assert.True(true);
+        }
+    }
+}
