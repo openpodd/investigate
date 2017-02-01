3302db8a8859508414e1284922d19068fe2a1175
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2017-02-01 13:51:11 +0700

    Add incident master page

---
+++ Investigate/IncidentMasterPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Investigate
+{
+	public partial class IncidentMasterPage : ContentPage
+	{
+	    public IncidentMasterDetailViewModel ViewModel { get; set; }
+
+	    public IncidentMasterPage()
+		{
+		    InitializeComponent();
+		    listView.ItemSelected += OnItemSelected;
+		}
+
+	    protected override void OnAppearing()
+	    {
+	        base.OnAppearing();
+	        listView.ItemsSource = ViewModel.PageItems;
+	    }
+
+	    private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+	    {
+	        var item = (MasterPageItem) e.SelectedItem;
+	        listView.SelectedItem = null;
+	        ViewModel.ChangePageCommand.Execute(item);
+	    }
+	}
+}
